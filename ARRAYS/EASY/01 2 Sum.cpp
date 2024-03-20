50ddcd3e63321798656eab78303af7cbffd00731
pranay <pranayharishchandra@gmail.com>
2024-03-20 06:39:51 +0530

    3 sum

--- ARRAYS/EASY/01 2 Sum.cpp
+++ ARRAYS/EASY/01 2 Sum.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 /** BRUTE : checking all the pairs
 Time Complexity: O(N2), where N = size of the array.
-Reason: There are two loops(i.e. nested) each running for approximately N times.
+Reason: There are two loops(i.e. nested) 
+each running for approximately N times.
 
 Space Complexity: O(1) as we are not using any extra space.
  */
@@ -25,13 +26,22 @@ vector<int> twoSum(int n, vector<int> &arr, int target) {
 
 /** OPTIMAL : using hashmap
 Time Complexity: O(N), where N = size of the array.
-Reason: The loop runs N times in the worst case and searching in a hashmap takes O(1) generally. So the time complexity is O(N).
+Reason: The loop runs N times in the worst case and searching 
+in a hashmap takes O(1) generally. So the time complexity is O(N).
 
-Note: In the worst case(which rarely happens), the unordered_map takes O(N) to find an element. In that case, the time complexity will be O(N2). If we use map instead of unordered_map, the time complexity will be O(N* logN) as the map data structure takes logN time to find an element.
+Note: In the worst case(which rarely happens), 
+the unordered_map takes O(N) to find an element. 
+In that case, the time complexity will be O(N2). 
+If we use map instead of unordered_map, 
+the time complexity will be O(N* logN) as the 
+map data structure takes logN time to find an element.
 
 Space Complexity: O(N) as we use the map data structure.
 
-Note: We have optimized this problem enough. But if in the interview, we are not allowed to use the map data structure, then we should move on to the following approach i.e. two pointer approach. This approach will have the same time complexity as the better approach.
+Note: We have optimized this problem enough. But if in the interview,
+ we are not allowed to use the map data structure, 
+ then we should move on to the following approach i.e. two pointer approach. 
+ This approach will have the same time complexity as the better approach.
  */
 vector<int> twoSum(int n, vector<int> &arr, int target) {
     unordered_map<int, int> mpp;
@@ -48,7 +58,8 @@ vector<int> twoSum(int n, vector<int> &arr, int target) {
 
 /** OPTIMAL : 2 pointer
 Time Complexity: O(N) + O(N*logN), where N = size of the array.
-Reason: The loop will run at most N times. And sorting the array will take N*logN time complexity.
+Reason: The loop will run at most N times. 
+And sorting the array will take N*logN time complexity.
 
 Space Complexity: O(1) as we are not using any extra space.
  */
---
+++ ARRAYS/MEDIUM/02 3 Sum.cpp
@@ -0,0 +1,114 @@
+#include <vector>
+#include <set>
+using namespace std;
+
+
+/** BRUTE : check all possible triplets
+Time Complexity: O(N3 * log(no. of unique triplets)),
+ where N = size of the array.
+Reason: Here, we are mainly using 3 nested loops. 
+And inserting triplets into the set takes 
+O(log(no. of unique triplets)) time complexity. 
+But we are not considering the time complexity of 
+sorting as we are just sorting 3 elements every time.
+
+Space Complexity: O(2 * no. of the unique triplets) 
+as we are using a set data structure and a list to store the triplets.
+ */
+vector<vector<int>> triplet(int n, vector<int> &arr) {
+    set<vector<int>> st;
+
+    // check all possible triplets:
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (arr[i] + arr[j] + arr[k] == 0) {
+                    vector<int> temp = {arr[i], arr[j], arr[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+
+    //store the set elements in the answer:
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
+
+/** OPTIMAL : using hashmap
+Time Complexity: O(N2 * log(no. of unique triplets)), 
+where N = size of the array.
+Reason: Here, we are mainly using 3 nested loops. 
+And inserting triplets into the set takes 
+O(log(no. of unique triplets)) time complexity. 
+But we are not considering the time complexity of 
+sorting as we are just sorting 3 elements every time.
+
+Space Complexity: O(2 * no. of the unique triplets) + O(N) 
+as we are using a set data structure 
+and a list to store the triplets 
+and extra O(N) for storing the array elements in another set.
+ */
+vector<vector<int>> triplet(int n, vector<int> &arr) {
+    set<vector<int>> st;
+
+    for (int i = 0; i < n; i++) {
+        set<int> hashset;
+        for (int j = i + 1; j < n; j++) {
+            //Calculate the 3rd element:
+            int third = -(arr[i] + arr[j]);
+
+            //Find the element in the set:
+            if (hashset.find(third) != hashset.end()) {
+                vector<int> temp = {arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(arr[j]);
+        }
+    }
+
+    //store the set in the answer:
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
+/** OPTIMAL : 1  fixed pointe and 2 pointer
+Time Complexity: O(NlogN)+O(N2), where N = size of the array.
+Reason: The pointer i, is running for approximately N times. And both the pointers j and k combined can run for approximately N times including the operation of skipping duplicates. So the total time complexity will be O(N2). 
+
+Space Complexity: O(no. of quadruplets), This space is only used to store the answer. We are not using any extra space to solve this problem. So, from that perspective, space complexity can be written as O(1).
+ */
+vector<vector<int>> triplet(int n, vector<int> &arr) {
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++) {
+        //remove duplicates:
+        if (i != 0 && arr[i] == arr[i - 1]) continue;
+
+        //moving 2 pointers:
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k) {
+            int sum = arr[i] + arr[j] + arr[k];
+            if (sum < 0) {
+                j++;
+            }
+            else if (sum > 0) {
+                k--;
+            }
+            else {
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                //skip the duplicates:
+                while (j < k && arr[j] == arr[j - 1]) j++;
+                while (j < k && arr[k] == arr[k + 1]) k--;
+            }
+        }
+    }
+    return ans;
+}
