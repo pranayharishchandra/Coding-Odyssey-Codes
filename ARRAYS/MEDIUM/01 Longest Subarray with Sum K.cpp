1d1600e8c1f7929195dfa5a22467c603e886d687
pranay <pranayharishchandra@gmail.com>
2024-03-24 22:48:02 +0530

    06 similar: Count Subarray sum Equals K

--- ARRAYS/MEDIUM/01 Longest Subarray with Sum K.cpp
+++ ARRAYS/MEDIUM/01 Longest Subarray with Sum K.cpp
@@ -189,3 +189,30 @@ int getLongestSubarray(vector<int>& arr, int k){
 	return maxi;
 }
 
+
+/**Similar question : Count Subarray sum Equals K
+
+ */
+#include <map>
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    map<int, int> mpp;
+    int preSum = 0, cnt = 0;
+
+    mpp[0] = 1; // Setting 0 in the map.
+    for (int i = 0; i < n; i++) {
+        // add current element to prefix Sum:
+        preSum += arr[i];
+
+        // Calculate x-k:
+        int remove = preSum - k;
+
+        // Add the number of subarrays to be removed:
+        cnt += mpp[remove];
+
+        // Update the count of prefix sum
+        // in the map.
+        mpp[preSum] += 1;
+    }
+    return cnt;
+}
