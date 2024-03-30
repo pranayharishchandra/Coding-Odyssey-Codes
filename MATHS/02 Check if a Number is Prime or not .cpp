461a9555088646b3744b1269b9618e07be93ba86
pranay <pranayharishchandra@gmail.com>
2024-03-30 22:50:57 +0530

    Check if a Number is Prime or not

---
+++ MATHS/02 Check if a Number is Prime or not .cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int isPrime(int n){
+        if (n <= 1) {
+            return false; // 0 and 1 are not prime
+        }
+        if (n <= 3) {
+            return true; // 2 and 3 are prime
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false; // Numbers divisible by 2 or 3 are not prime
+        }
+        // Check divisibility by numbers of the form 6k +/- 1 up to sqrt(n)
+       for (int i = 6; i * i <= n; i += 6) {
+            if (n % (i-1) == 0 || n % (i+1) == 0) {
+                return false;
+            }
+        }
+        return true; // If no divisors found, number is prime
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.isPrime(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
