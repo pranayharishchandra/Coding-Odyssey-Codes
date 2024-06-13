e2d6c9124d7738a085fb44fef138070100ae69f6
pranay <pranayharishchandra@gmail.com>
2024-06-13 16:05:10 +0530

    g

--- 05 GREEDY/02 MEDIUM/N Meetings in 1 Room.cpp
+++ 05 GREEDY/02 MEDIUM/N Meetings in 1 Room.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution
 {
     public:
@@ -10,7 +14,7 @@ class Solution
     };
     
     //comparator
-    static bool comparator(struct meeting m1, struct meeting m2){
+    static bool comparator(struct meeting m1,  meeting m2){
         if(m1.end < m2.end) return true;
         else if(m1.end > m2.end) return false;
         else if(m1.pos < m2.pos) return true;
